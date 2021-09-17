acc2e66dbf79d161c78e26f39fa5984467937800
NikGapon <45200250+NikGapon@users.noreply.github.com>
2021-09-17 19:33:46 +0400

    lab1and2inclass

---
+++ lab2/test2/test2.cpp
@@ -0,0 +1,327 @@
+﻿// test2.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "test2.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_TEST2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TEST2));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TEST2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TEST2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+
+            
+
+            int x1 = 10, y1 = 100;
+            int x2 = 300, y2 = 100;
+            int i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                y1 = y1 - 5;
+                y2 = y2 + 10;
+                i++;
+            } while (i < 16);
+            
+
+            x1 = 100, y1 = 300;
+            x2 = 100, y2 = 500;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 - 5;
+                y1 = y1 + 5;
+                y2 = y2 - 5;
+                x2 = x2 + 5;
+                
+                i++;
+            } while (i < 16);
+
+            
+            x1 = 400, y1 = 100;
+            x2 = 500, y2 = 100;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                
+                y1 = y1 - 5;
+                
+                x2 = x2 - 5;
+
+                i++;
+            } while (i < 21);
+
+
+
+            x1 = 400, y1 = 115;
+            x2 = 400, y2 = 215;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 - 5;
+                y1 = y1 + 5;
+                y2 = y2 - 5;
+                
+
+                i++;
+            } while (i < 16);
+
+
+
+
+            x1 = 600, y1 = 110;
+            x2 = 700, y2 = 10;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 5;
+                
+                y2 = y2 + 6;
+
+
+                i++;
+            } while (i < 16);
+
+
+            x1 = 600, y1 = 115;
+            x2 = 600, y2 = 215;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 3;
+                y1 = y1 + 3;
+                y2 = y2 - 3;
+                x2 = x2 - 3;
+
+                i++;
+            } while (i < 16);
+
+            x1 = 600, y1 = 225;
+            x2 = 600, y2 = 325;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 5;
+                
+                y2 = y2 - 5;
+                
+
+                i++;
+            } while (i < 16);
+
+            x1 = 400, y1 = 300;
+            x2 = 400, y2 = 400;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 5;
+                y1 = y1 - 3;
+                x2 = x2 + 5;
+                y2 = y2 + 3;
+                i++;
+            } while (i < 15);
+
+
+            x1 = 10, y1 = 600;
+            x2 = 10, y2 = 700;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 5;
+                y1 = y1 + 7;
+                x2 = x2 + 5;
+                y2 = y2 - 7;
+                i++;
+            } while (i < 15);
+
+
+            x1 = 100, y1 = 600;
+            x2 = 150, y2 = 700;
+            i = 0;
+            do {
+                MoveToEx(hdc, x1, y1, NULL);
+                LineTo(hdc, x2, y2);
+                x1 = x1 + 5;
+                y1 = y1 + 5;
+                x2 = x2 + 3;
+                y2 = y2 - 5;
+                i++;
+            } while (i < 20);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
