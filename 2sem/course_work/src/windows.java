5ca6266972367c08bf1635dd12b23769e25770ca
NikGapon <45200250+NikGapon@users.noreply.github.com>
2022-05-18 20:19:08 +0400

    new upd

--- 2sem/course_work/src/windows.java
+++ 2sem/course_work/src/windows.java
@@ -10,31 +10,41 @@ public class windows extends JFrame{
 
         ArrayList<Integer> main_list = new ArrayList<>();
 
-        //ArrayList<String> cash = new ArrayList<>();
-
+        ArrayList<Integer> cash_left = new ArrayList<>();
+        ArrayList<Integer> cash_mid = new ArrayList<>();
+        ArrayList<Integer> cash_right = new ArrayList<>();
         Timer timer;
-        JFrame frame = new JFrame("binary search");
+        //JFrame frame = new JFrame("binary search");
         String text_add = "";
         //String text_second = "";
+
 
         int result_number_one;
         boolean Animation = false;
         int result_number_two;
-        JLabel main_laibel = new JLabel("");
+
         //JLabel second_laibel = new JLabel("");
 
-        int Moment_left = 100;
+        int Moment_left = 0;
         int Moment_mid = 0;
         int Moment_right = 0;
+
+        int go_left = 0;
+        int go_mid = 0;
+        int go_right = 0;
+        public windows(){
+            super("binary search");
+            start();
+        }
         public void start() {
-
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(1000, 400);
-            frame.setLocationRelativeTo(null);
-
-
             Paint paint = new Paint();
-            frame.add(paint);
+            add(paint);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(1000, 400);
+            setLocationRelativeTo(null);
+
+
+
 
             JPanel panel = new JPanel(new FlowLayout(3));
             JTextField textField = new JTextField(20);
@@ -43,8 +53,8 @@ public class windows extends JFrame{
             JButton button_search = new JButton("search");
             //JButton button_remove = new JButton("remove");
             JLabel reuelt_laibel = new JLabel("");
-
-            main_laibel.setBounds(500, 200, 150, 50);
+            //JLabel main_laibel = new JLabel("");
+            //main_laibel.setBounds(500, 200, 150, 50);
             //second_laibel.setBounds(500 - text_add.length() * 2, 210, 1000, 50);
 
             button_add.addActionListener(new ActionListener() {
@@ -54,9 +64,9 @@ public class windows extends JFrame{
                     text_add += Integer.parseInt(textField.getText());
                     text_add += "  ";
 
-                    main_laibel.setBounds(500 - text_add.length() * 2, 200, 1000, 50);
-
-                    main_laibel.setText(text_add);
+                    //main_laibel.setBounds(500 - text_add.length() * 2, 200, 1000, 50);
+                    Animation = true;
+                    //main_laibel.setText(text_add);
 
                     System.out.println(main_list);
 
@@ -70,16 +80,14 @@ public class windows extends JFrame{
                     //second_laibel.setBounds(500 - text_add.length() * 2, 210, 1000, 50);
 
                     //second_laibel.setText(text_second);
-
                     binarySearch(key);
-
                     Animation = true;
                     reuelt_laibel.setText("Результат прошлого успешного поиска: " + result_number_one + "   На позиции: " + result_number_two);
                 }
             });
 
 
-            JPanel TextPanel = new JPanel();
+            //JPanel TextPanel = new JPanel();
 
 
             panel.add(button_search);
@@ -88,12 +96,12 @@ public class windows extends JFrame{
             panel.add(textField);
             panel.add(reuelt_laibel);
 
-            frame.add(main_laibel);
+            //getContentPane().add(BorderLayout.WEST, main_laibel);
             //frame.add(second_laibel);
-            frame.add(panel);
+            getContentPane().add(BorderLayout.NORTH,panel);
 
 
-            frame.setVisible(true);
+            setVisible(true);
         }
 
         void binarySearch(int key) {
@@ -103,44 +111,13 @@ public class windows extends JFrame{
 
 
             while (low <= high) {
-                //text_second = "";
+
                 int mid = (low + high) >>> 1;
                 int midVal = main_list.get(mid);
-                /*
-                int x = text_add.indexOf("" + main_list.get(low));
-                System.out.println(x);
-                for (int i = 0; i <= x; i++) {
-                    text_second += " ";
-                }
-                text_second += "↑min";
-                x = text_add.indexOf("" + main_list.get(mid));
-                for (int i = 0; i <= x - 4; i++) {
-                    text_second += " ";
-                }
-                text_second += "↑mid";
-                x = text_add.indexOf("" + main_list.get(high));
-                for (int i = 0; i <= x - 8; i++) {
-                    text_second += " ";
-                }
-                text_second += "↑high";*/
 
-                //System.out.println(text_add);
-                //System.out.println(text_second);
-
-
-                //second_laibel.setText(text_second);
-                //cash.add(text_second);
-            /*SwingUtilities.updateComponentTreeUI(frame);
-            frame.invalidate();
-            frame.validate();
-            frame.repaint();*/
-
-                /*try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-
-                }*/
-
+                int x = text_add.indexOf("" + main_list.get(low)) + 1;
+                //System.out.println(x);
+                cash_left.add(500 - text_add.length() * 2 + x);
 
                 if (midVal < key) {
                     low = mid + 1;
@@ -153,44 +130,98 @@ public class windows extends JFrame{
                     //return ;
                 }
             }
-            //result_number_two = -1;
-            //result_number_one = -1;
+
         }
 
         public class Paint extends JComponent implements ActionListener {
             public Paint(){
                 super();
 
-                timer = new Timer(100, this);
+                timer = new Timer(10, this);
                 timer.start();
             }
 
             @Override
             public void actionPerformed(ActionEvent e){
-                if(Animation){
+                int x;
+                if(Animation) {
+                    if (Moment_left == go_left && Moment_left == go_mid && Moment_right == go_right) {
+                        if (cash_left.size() > 0 || cash_mid.size() > 0 || cash_right.size() > 0) {
+
+                            if (cash_left.size() > 0) {
+                                //System.out.println(cash_left);
+                                x = cash_left.get(0);
+                                Moment_left = 500 - text_add.length() * 2;
+                                cash_left.remove(0);
+                                go_left = x + 500 - text_add.length() * 2;
+
+                            }
+                            if (cash_mid.size() > 0) {
+                                Moment_mid = 500 - text_add.length();
+                                x = cash_mid.get(0);
+                                cash_mid.remove(0);
+                                go_mid = x + 500 - text_add.length() * 2;
+
+                            }
+                            if (cash_right.size() > 0) {
+                                Moment_right = 500 + text_add.length() * 2;
+                                x = cash_right.get(0);
+                                cash_right.remove(0);
+                                go_right = x + 500 - text_add.length() * 2;
+
+                            }
+                        } else {
+                            Animation = false;
+                        }
+                    }
+
+                    //System.out.println("dadadada");
+                    //System.out.println(Moment_left + "  " + go_left);
+                    if (Moment_left > go_left) {
+
+                        Moment_left -= 1;
+                    }
+                    if (Moment_left < go_left) {
+
+                        Moment_left += 1;
+                    }
+
+                    if (Moment_mid > go_mid) {
+                        Moment_mid -= 1;
+                    }
+                    if (Moment_mid < go_mid) {
+                        Moment_mid += 1;
+                    }
+
+                    if (Moment_right > go_right) {
+                        Moment_right -= 1;
+                    }
+                    if (Moment_right < go_right) {
+                        Moment_right += 1;
+                    }
+
 
                     //Animation = false;
+                    repaint();
+                    timer.setDelay(10);
                 }
-
-
-                repaint();
-                timer.setDelay(100);
             }
             public void PaintElements(Graphics g2d){
                 g2d.setColor(Color.BLUE);
+                //System.out.println(1);
+                g2d.drawLine(Moment_left, 215, Moment_left - 50, 215);
+                g2d.drawLine(Moment_left, 215, Moment_left - 50, 265);
+                g2d.drawLine(Moment_left, 215, Moment_left, 265);
 
-                g2d.drawLine(Moment_left, 210, Moment_left - 50, 210);
-                g2d.drawLine(Moment_left, 210, Moment_left - 50, 160);
-                g2d.drawLine(Moment_left, 210, Moment_left, 160);
-
-
-                g2d.setColor(new Color(179, 255, 179));
-                g2d.fillRect(100, 100, 100, 100);
+                g2d.setColor(Color.BLACK);
+                g2d.drawString(text_add, 500 - text_add.length() * 2, 210);
+                //g2d.setColor(new Color(170, 255, 0));
+                //g2d.fillRect(100, 100, 100, 100);
             }
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
-                validate ();
-                System.out.println("dadadada");
+
+                //System.out.println("dadadada");
                 Graphics2D g2d = (Graphics2D) g;
                 PaintElements(g2d);
 
@@ -199,8 +230,8 @@ public class windows extends JFrame{
 
         public static void main(String[] args) {
             windows start = new windows();
-            start.start();
-            start.pack();
+            //start.start();
+            //start.pack();
             start.setVisible(true);
 
 
