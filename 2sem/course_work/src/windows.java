6f5d567eb2bb416dcb0f42828f57f6e136a6b8c9
NikGapon <45200250+NikGapon@users.noreply.github.com>
2022-05-26 22:18:30 +0400

    sem2 course_work big upd step and remove_button

--- 2sem/course_work/src/windows.java
+++ 2sem/course_work/src/windows.java
@@ -17,7 +17,7 @@ public class windows extends JFrame{
         //JFrame frame = new JFrame("binary search");
         String text_add = "";
         //String text_second = "";
-        int cof = 5;
+        int coefficient_skale = 6;
 
         int result_number_one;
         boolean Animation = false;
@@ -25,13 +25,15 @@ public class windows extends JFrame{
 
         //JLabel second_laibel = new JLabel("");
 
-        int Moment_left = 0;
-        int Moment_mid = 0;
-        int Moment_right = 0;
+        int Moment_left = -50;
+        int Moment_mid = -50;
+        int Moment_right = -50;
 
-        int go_left = 0;
-        int go_mid = 0;
-        int go_right = 0;
+        boolean searh_move_start = false;
+
+        int go_left = -50;
+        int go_mid = -50;
+        int go_right = -50;
         public windows(){
             super("binary search");
             start();
@@ -51,8 +53,8 @@ public class windows extends JFrame{
 
             JButton button_add = new JButton("add");
             JButton button_search = new JButton("search");
-            //JButton button_remove = new JButton("remove");
-            JLabel reuelt_laibel = new JLabel("");
+            JButton button_remove = new JButton("remove");
+            JLabel resuelt_laibel = new JLabel("");
             //JLabel main_laibel = new JLabel("");
             //main_laibel.setBounds(500, 200, 150, 50);
             //second_laibel.setBounds(500 - text_add.length() * 2, 210, 1000, 50);
@@ -82,19 +84,38 @@ public class windows extends JFrame{
                     //second_laibel.setText(text_second);
                     binarySearch(key);
                     Animation = true;
-                    reuelt_laibel.setText("Результат прошлого успешного поиска: " + result_number_one + "   На позиции: " + result_number_two);
+                    resuelt_laibel.setText("Результат прошлого успешного поиска: " + result_number_one + "   На позиции: " + result_number_two);
                 }
             });
 
+            button_remove.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    main_list.clear();
+                    cash_left.clear();
+                    cash_mid.clear();
+                    cash_right.clear();
+                    resuelt_laibel.setText("");
+                    Moment_left = -50;
+                    Moment_mid = -50;
+                    Moment_right = -50;
+                    searh_move_start = false;
+                    go_left = -50;
+                    go_mid = -50;
+                    go_right = -50;
+                    Animation = true;
+                    text_add = "";
+                }
+            });
 
             //JPanel TextPanel = new JPanel();
 
-
+            panel.add(button_remove);
             panel.add(button_search);
             panel.add(button_add);
-            //panel.add(button_remove);
+
             panel.add(textField);
-            panel.add(reuelt_laibel);
+            panel.add(resuelt_laibel);
 
             //getContentPane().add(BorderLayout.WEST, main_laibel);
             //frame.add(second_laibel);
@@ -115,11 +136,17 @@ public class windows extends JFrame{
                 int mid = (low + high) >>> 1;
                 int midVal = main_list.get(mid);
 
-                int x = text_add.indexOf(" " + main_list.get(low)) + 1;
-                cash_left.add(200 + x * cof);
+                int x = text_add.indexOf(" " + main_list.get(low));
+                cash_left.add(200 + x * coefficient_skale);
 
-                x = text_add.indexOf(" " + main_list.get(high)) + 1;
-                cash_right.add(200 + x * cof);
+
+                x = text_add.indexOf(" " + main_list.get(high));
+                cash_right.add(200 + x * coefficient_skale);
+
+
+
+                x = text_add.indexOf(" " + main_list.get(mid));
+                cash_mid.add(200 + x * coefficient_skale);
 
                 if (midVal < key) {
                     low = mid + 1;
@@ -128,13 +155,11 @@ public class windows extends JFrame{
                 } else {
                     result_number_two = mid;
                     result_number_one = main_list.get(mid);
-                    x = text_add.indexOf(" " + main_list.get(mid)) + 1;
-                    cash_mid.add(200 + x * cof);
+                    x = text_add.indexOf(" " + main_list.get(mid));
+                    cash_mid.add(200 + x * coefficient_skale);
                     break;
-                    //return ;
                 }
             }
-
         }
 
         public class Paint extends JComponent implements ActionListener {
@@ -149,45 +174,53 @@ public class windows extends JFrame{
             public void actionPerformed(ActionEvent e){
                 int x;
                 if(Animation) {
-                    if (Moment_left == go_left && Moment_left == go_mid && Moment_right == go_right) {
+                    if (Moment_left == go_left && Moment_mid == go_mid && Moment_right == go_right) {
                         if (cash_left.size() > 0 || cash_mid.size() > 0 || cash_right.size() > 0) {
+                            if (cash_left.size() == cash_right.size()){
+                                if (cash_left.size() > 0) {
 
-                            if (cash_left.size() > 0) {
+                                    x = cash_left.get(0);
+                                    if (searh_move_start == false) {
+                                        Moment_left = 200;
+                                    }
+                                    cash_left.remove(0);
+                                    go_left = x;
 
-                                x = cash_left.get(0);
-                                //System.out.println(x);
-                                Moment_left = 200;
-                                cash_left.remove(0);
-                                go_left = x;
 
-                            }
-                             if (cash_mid.size() > 0) {
-                                Moment_mid = 500;
-                                x = cash_mid.get(0);
-                                cash_mid.remove(0);
-                                go_mid = x;
+                                }
+                                if (cash_mid.size() > 0) {
+                                    if (searh_move_start == false) {
+                                        Moment_mid = (text_add.length() / 2) * coefficient_skale  + 200;
+                                    }
+                                    x = cash_mid.get(0);
+                                    cash_mid.remove(0);
+                                    go_mid = x;
 
-                            }
-                             if (cash_right.size() > 0) {
-                                Moment_right = 900;
-                                x = cash_right.get(0);
-                                cash_right.remove(0);
-                                go_right = x;
+                                }
+                                if (cash_right.size() > 0) {
+                                    if (searh_move_start == false) {
+                                        Moment_right = text_add.length() * coefficient_skale + 200;
+                                    }
+                                    x = cash_right.get(0);
+                                    cash_right.remove(0);
+                                    go_right = x;
 
+                                }
+                                searh_move_start = true;
                             }
                         }
                         else {
                             Animation = false;
                         }
                     }
+
+
                 }
-                    //System.out.println("dadadada");
-                    //System.out.println(Moment_left + "  " + go_left);
+
                     if (Moment_left > go_left) {
                         Moment_left -= 1;
                     }
                     else if (Moment_left < go_left) {
-
                         Moment_left += 1;
                     }
                     else if (Moment_right > go_right) {
@@ -219,6 +252,9 @@ public class windows extends JFrame{
                 g2d.drawLine(Moment_left, 215, Moment_left - 50, 265);
                 g2d.drawLine(Moment_left, 215, Moment_left, 265);
 
+                g2d.drawLine(25, 25, 0, 25);
+                g2d.drawLine(25, 25, 0, 50);
+                g2d.drawLine(25, 25, 25, 50);
 
 
                 g2d.setColor(Color.RED);
@@ -227,17 +263,26 @@ public class windows extends JFrame{
                 g2d.drawLine(Moment_right, 200, Moment_right + 50, 150);
                 g2d.drawLine(Moment_right, 200, Moment_right, 150);
 
+                g2d.drawLine(0, 80, 25, 80);
+                g2d.drawLine(0, 80, 25, 55);
+                g2d.drawLine(0, 80, 25, 55);
+
                 g2d.setColor(Color.BLACK);
 
                 g2d.drawLine(Moment_mid, 200, Moment_mid + 25, 175);
                 g2d.drawLine(Moment_mid, 200, Moment_mid - 25, 175);
                 g2d.drawLine(Moment_mid, 200, Moment_mid, 200);
 
+                g2d.drawLine(15, 100, 30, 85);
+                g2d.drawLine(15, 100, 0, 85);
 
                 g2d.setColor(Color.BLACK);
                 g2d.drawString(text_add, 200, 210);
-                //g2d.setColor(new Color(170, 255, 0));
-                //g2d.fillRect(100, 100, 100, 100);
+
+                g2d.drawString("- левая граница массива", 30, 40);
+                g2d.drawString("- правая граница массива", 30, 70);
+                g2d.drawString("- середина массива", 30, 95);
+
             }
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
